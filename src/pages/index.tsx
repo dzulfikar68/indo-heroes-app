af2692ee1bf78829dff4f022c4376adb3fb42d78
Dzulfikar Fauzi <dzulfikar68.fauzi@gmail.com>
2025-02-09 22:43:56 +0700

    add fixed tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      <div>
+      {/* <Navbar/> */}
+      {/* <Head>
+        <title>Home</title>
+      </Head> */}
+      <div className='text-3xl font-bold underline'>
       hello world! fikar
       </div>
     </div>
